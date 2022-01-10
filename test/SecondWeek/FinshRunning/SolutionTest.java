f475c0187955ffb27fb040e9e3d0117dfd94ce2c
ManduTheCat <krocd@naver.com>
2022-01-10 23:35:10 +0900

    solve using hashmap mathod list = keyset, map.get(), map.push()

--- test/SecondWeek/FinshRunning/SolutionTest.java
+++ test/SecondWeek/FinshRunning/SolutionTest.java
@@ -7,12 +7,12 @@ import static org.junit.Assert.*;
 public class SolutionTest {
 
 	@Test
-	public void solution() {
+	public void test() {
 		String []par = {"leo", "kiki", "eden"};
 		String []com = {"eden", "kiki"};
 		String []par1 = {"marina", "josipa", "nikola", "vinko", "filipa"};
 		String []com1 = {"josipa", "filipa", "marina", "nikola"};
-		SecondWeek.FinshRunning.Solution test = new SecondWeek.FinshRunning.Solution();
+		Solution test = new Solution();
 		assertEquals(test.solution(par, com),"leo");
 		assertEquals(test.solution(par1, com1),"vinko");
 
