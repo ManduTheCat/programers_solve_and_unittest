385968a59b3a8daae2c6174a230a34f4bb96bed6
ManduTheCat <krocd@naver.com>
2022-01-12 21:42:34 +0900

    try

---
+++ src/FirstWeek/visitLength/Main.java
@@ -0,0 +1,10 @@
+package FirstWeek.visitLength;
+
+public class Main {
+	public static void main(String[] args) {
+		Solution test = new Solution();
+		String input1 ="ULURRDLLU";
+		test.solution(input1);
+	}
+}
+
