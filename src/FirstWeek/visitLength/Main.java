44f8d17f6cd2ae0cb50a198fe75a9d3c8b0cdc6f
ManduTheCat <krocd@naver.com>
2022-01-13 04:30:59 +0900

    sovle!!!

--- src/FirstWeek/visitLength/Main.java
+++ src/FirstWeek/visitLength/Main.java
@@ -2,7 +2,6 @@ package FirstWeek.visitLength;
 
 public class Main {
 	public static void main(String[] args) {
-		Solution test = new Solution();
 		Solution test1 = new Solution();
 		String input1 ="ULURRDLLU";
 		String input2 = "LULLLLLLU";
