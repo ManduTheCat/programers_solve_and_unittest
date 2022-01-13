7d8e5365c5c9bb1bf06c6145514c4e7dc189de41
ManduTheCat <krocd@naver.com>
2022-01-13 16:51:22 +0900

    clean

--- src/FirstWeek/visitLength/Solution.java
+++ src/FirstWeek/visitLength/Solution.java
@@ -1,12 +1,11 @@
 package FirstWeek.visitLength;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Objects;
 
 public class Solution {
 
-	public static class Pair<T, S>{
+	public class Pair<T, S>{
 		T i;
 		S j;
 		public Pair() {
@@ -23,23 +22,20 @@ public class Solution {
 		private int curJ;
 		private int nI;
 		private int nJ;
-		public int[] res;
 
 		public Path(Pair<Integer, Integer> cur, Pair<Integer, Integer> next){
 			this.curI = cur.i;
 			this.curJ = cur.j;
 			this.nI = next.i;
 			this.nJ = next.j;
-			this.res = new int[]{this.curI, this.curJ, this.nI, this.nJ};
 		}
-
 
 		@Override
 		public boolean equals(Object o) {
 			if (this == o) return true;
 			if (o == null || getClass() != o.getClass()) return false;
 			Path path = (Path) o;
-			return curI == path.curI && curJ == path.curJ && nI == path.nI && nJ == path.nJ && Arrays.equals(res, path.res);
+			return curI == path.curI && curJ == path.curJ && nI == path.nI && nJ == path.nJ ;
 		}
 
 		@Override
@@ -64,44 +60,50 @@ public class Solution {
 			nextPoint = move(idx, point);
 			Path go = new Path(point, nextPoint);
 			Path back = new Path(nextPoint, point);
-			//양뱡향 넣기
+
+			// 가는 경로 오는 경로 false로 초기화
 			pathCheck.put(go, false);
 			pathCheck.put(back , false);
-			// point 갱신
 			point = nextPoint;
 		}
+
 		point.set(5,5);
-		int res = 0;
+		int pathCount = 0;
 		for(int i = 0; i < dirs.length(); i++){
 			int idx = command.get(dirs.charAt(i));
 			nextPoint = move(idx, point);
 			Path go = new Path(point, nextPoint);
 			Path back = new Path(nextPoint, point);
+
+			// 입력된 명령되로 진행되면서 pathCheck(좌표 , false) true 로 변환 경로 갯수를 센다.
 			if(!pathCheck.get(go) && !pathCheck.get(back)){
 				pathCheck.put(go, true);
 				pathCheck.put(back,true);
-				res++;
+				pathCount++;
+
+				// 경로가 범위를 넘어서면 point = nextPoint 부분 때문에 같은 좌표를 반복 하여 카운트가 올라간다 이를 처리
 				if((go.curI == back.curI) && (go.curJ == back.curJ) && (go.nI == back.nI) && (go.nJ == back.nJ)) {
-					res--;
+					pathCount--;
 				}
 			}
 			point = nextPoint;
 		}
-		return res;
+		return pathCount;
 	}
 	Pair<Integer, Integer> move(int idx, Pair<Integer, Integer> point){
 		Pair<Integer, Integer> pointTemp = new Pair<>();
 		pointTemp.set(point.i, point.j);
-		// 이동좌표한 좌표를 넘기
 		int[] dj = {0,0,1,-1};
 		int[] di = {-1,1,0,0};
-		// 포인트가 범위를 넘어가면 하지말아야한다 넘어가면 좌표에서 더하지 않는다..
+
+		// 포인트가 범위를 넘어가면 하지말아야한다 넘어가면 좌표에서 더하지 않는다.
 		pointTemp.i += di[idx];
 		pointTemp.j += dj[idx];
 
 		// 이전에 움직인 범위에 의해 캐릭터의 위치가 지도를 벗어났을 경우
 		if(pointTemp.i < 0 || pointTemp.j < 0 || pointTemp.i > 10 || pointTemp.j > 10){
-			// 다시 캐릭터를 전의 위치로 이동
+
+			// 다시 전의 위치로 이동
 			pointTemp.i -= di[idx];
 			pointTemp.j -= dj[idx];
 		}
