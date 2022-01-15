44622073eb5dc103301bfb770d27f0214d538b7d
ManduTheCat <krocd@naver.com>
2022-01-15 17:41:12 +0900

    change move function 범위 넘어가면 무시하도록

---
+++ src/ThirdWeek/MoreHot/Main.java
@@ -0,0 +1,11 @@
+package ThirdWeek.MoreHot;
+
+public class Main {
+	public static void main(String[] args) {
+		Solution test = new Solution();
+		int [] input1 = {1, 2, 3, 9, 10, 12};
+		int k1 = 7;
+		test.solution(input1, k1);
+	}
+
+}
---
+++ src/ThirdWeek/MoreHot/Solution.java
@@ -0,0 +1,8 @@
+package ThirdWeek.MoreHot;
+
+public class Solution {
+	public int solution(int[] scoville, int K) {
+		int answer = 0;
+		return answer;
+	}
+}
