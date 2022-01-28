fdc1ef665ad931e2d05d6546dcc2cfb832c2dcd9
ManduTheCat <krocd@naver.com>
2022-01-28 16:44:19 +0900

    :construction: 이중 어래이리스틀 활용한 수들의 합을 기록하는 풀이

--- src/FourthWeek/TargeNum/Main.java
+++ src/FourthWeek/TargeNum/Main.java
@@ -4,7 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		Solution test = new Solution();
 		int [] intpu1 = {1, 1, 1, 1, 1};
+		int [] intpu2 = {4, 1, 2, 1};
+		int target2 = 4;
 		int target1 = 3;
 		System.out.println(test.solution(intpu1, target1));
+		System.out.println(test.solution(intpu2, target2));
 	}
 }
--- src/FourthWeek/TargeNum/Solution.java
+++ src/FourthWeek/TargeNum/Solution.java
@@ -1,14 +1,22 @@
 package FourthWeek.TargeNum;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class Solution {
-	int [] pathCheck = new int[20];
+
 	public int solution(int[] numbers, int target) {
 	// 모든 수는 연산을 무조건 해야한다 dfs?
+		ArrayList<ArrayList<Integer>> numSum = new ArrayList<>();
+		int [][] numbersArr = new int[numbers.length][2];
+		for(int i = 0; i < numbers.length; i++){
+			numbersArr[i][0] = numbers[i];
+			numbersArr[i][1] = numbers[i] * -1;
+		}
+		// 초기값 지정 해야한다.
+
 
 		return 0;
-	}
-	public void dfs (int node, int sum){
-
 	}
 }
 
